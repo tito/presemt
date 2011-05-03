c609c85ac90e1e90b867449b8073a76d665ec6ea
Mathieu Virbel <mat@kivy.org>
2011-05-03 19:30:00 +0200

    rework selector item to add trash icon in the next commit

--- presemt/screens/project.py
+++ presemt/screens/project.py
@@ -2,19 +2,54 @@ from . import Screen
 from os import listdir
 from os.path import join, exists
 from datetime import datetime
-from functools import partial
 
 from document import Document
 from kivy.lang import Builder
 from kivy.graphics.texture import Texture
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty
+from kivy.factory import Factory
+from kivy.animation import Animation
+from kivy.uix.floatlayout import FloatLayout
+
+class ModalSelect(FloatLayout):
+
+    alpha = NumericProperty(0.)
+
+    filename = StringProperty(None)
+
+    app = ObjectProperty(None)
+
+    def on_touch_down(self, touch):
+        super(ModalSelect, self).on_touch_down(touch)
+        return True
+
+Factory.register('ModalSelect', cls=ModalSelect)
 
 class SelectorScreen(Screen):
+
+    modalselect = ObjectProperty(None, allownone=True)
+
     def __init__(self, **kwargs):
         super(SelectorScreen, self).__init__(**kwargs)
 
     def on_parent(self, instance, value):
         if value:
             self.refresh()
+
+    def ask_load(self, filename):
+        if self.modalselect:
+            self.leave_load()
+        else:
+            self.modalselect = modalselect = ModalSelect(
+                app=self, filename=filename)
+            self.add_widget(modalselect)
+            Animation(alpha=1, d=.5, t='out_cubic').start(modalselect)
+
+    def leave_load(self):
+        if not self.modalselect:
+            return
+        self.remove_widget(self.modalselect)
+        self.modalselect = None
 
     def refresh(self):
         self.view.clear_widgets()
@@ -57,20 +92,21 @@ class SelectorScreen(Screen):
             title = 'No preview available'
 
         item = Builder.template('SelectorItem',
+            app=self,
             title=title,
             time=dt.strftime('%d/%m/%y %H:%M'),
             tex0=tex0, tex1=tex1, tex2=tex2,
             slide_count=len(slides),
             obj_count=len(list(doc.objects)),
-            func_edit=partial(self.do_edit, filename),
-            func_play=partial(self.do_play, filename)
-        )
+            filename=filename)
         self.view.add_widget(item)
 
     def do_edit(self, filename):
+        self.leave_load()
         print 'do_edit', filename
         self.app.edit_project(filename)
 
     def do_play(self, filename):
+        self.leave_load()
         print 'do_play', filename
         self.app.play_project(filename)
