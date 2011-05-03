f887335b7c97abd0d23f727bc4e0597b7716e097
Mathieu Virbel <mat@kivy.org>
2011-05-04 00:59:01 +0200

    add help text, but look like it just doesn't work.

--- presemt/screens/project.py
+++ presemt/screens/project.py
@@ -7,43 +7,75 @@ from document import Document
 from kivy.lang import Builder
 from kivy.graphics.texture import Texture
 from kivy.properties import ObjectProperty, NumericProperty, StringProperty
-from kivy.factory import Factory
 from kivy.animation import Animation
 from kivy.uix.floatlayout import FloatLayout
 
-class ModalSelect(FloatLayout):
+_help_about = '''
+PreseMT is a presentation tool build in top of Kivy.
+It's very cool, you should use it.
 
+Check Kivy for more information at http://kivy.org/
+'''
+
+_help_selector = '''
+The selector is the first screen you'll see in PreseMT.
+They are 3 buttons at the top right that represent :
+
+    * Info icon: Information / Help
+    * Refresh icon: Reread the list of project
+    * New icon: Create a new presentation
+
+The middle list represent all the projects already edited once.
+When you click on a presentation, you'll have 4 icons:
+
+    * Back icon: Back to the selector screen
+    * Trash: Delete the selected project
+    * Edit: Edit the selected project
+    * View: View the selected project
+'''
+
+_help_main = '''
+The interface is divided in 4 zones:
+
+    * Left toolbar / top: You can add text and image object by
+      clicking on one of theses icons.
+    * Left toolbar / bottom:
+      - Save the project (only when changes)
+      - View the presentation
+      - Lock all objects, but not the plane
+      - Back to the selector
+    * Right toolbar:
+      - Create a new Slide
+      - Refresh all the slides captures
+'''
+
+
+class Modal(FloatLayout):
     alpha = NumericProperty(0.)
-
-    filename = StringProperty(None)
-
     app = ObjectProperty(None)
-
     def on_touch_down(self, touch):
-        super(ModalSelect, self).on_touch_down(touch)
+        super(Modal, self).on_touch_down(touch)
         return True
 
-Factory.register('ModalSelect', cls=ModalSelect)
-
-class ModalConfirm(FloatLayout):
-
-    alpha = NumericProperty(0.)
-
+class ModalSelect(Modal):
     filename = StringProperty(None)
 
-    app = ObjectProperty(None)
+class ModalConfirm(Modal):
+    filename = StringProperty(None)
 
-    def on_touch_down(self, touch):
-        super(ModalConfirm, self).on_touch_down(touch)
-        return True
+class ModalHelp(Modal):
+    text_about = StringProperty(_help_about)
+    text_help_selector = StringProperty(_help_selector)
+    text_help_main = StringProperty(_help_main)
 
-Factory.register('ModalConfirm', cls=ModalConfirm)
 
 class SelectorScreen(Screen):
 
     modalselect = ObjectProperty(None, allownone=True)
 
     modalconfirm = ObjectProperty(None, allownone=True)
+
+    modalhelp = ObjectProperty(None, allownone=True)
 
     def __init__(self, **kwargs):
         super(SelectorScreen, self).__init__(**kwargs)
@@ -66,6 +98,20 @@ class SelectorScreen(Screen):
             return
         self.remove_widget(self.modalselect)
         self.modalselect = None
+
+    def show_help(self):
+        if self.modalhelp:
+            return
+        self.modalhelp = modalhelp = ModalHelp(app=self)
+        self.add_widget(modalhelp)
+        Animation(alpha=1, d=.5, t='out_cubic').start(modalhelp)
+
+    def leave_help(self):
+        if not self.modalhelp:
+            return
+        self.remove_widget(self.modalhelp)
+        self.modalhelp = None
+
 
     def delete_project(self, filename, force=False):
         if force:
