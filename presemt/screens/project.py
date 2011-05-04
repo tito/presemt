4c01186add108ec5eaa1b4b959a91bc9277a4dcf
Mathieu Virbel <mat@kivy.org>
2011-05-04 10:32:14 +0200

    rework to add help screen + source

--- presemt/screens/project.py
+++ presemt/screens/project.py
@@ -10,46 +10,6 @@ from kivy.properties import ObjectProperty, NumericProperty, StringProperty
 from kivy.animation import Animation
 from kivy.uix.floatlayout import FloatLayout
 
-_help_about = '''
-PreseMT is a presentation tool build in top of Kivy.
-It's very cool, you should use it.
-
-Check Kivy for more information at http://kivy.org/
-'''
-
-_help_selector = '''
-The selector is the first screen you'll see in PreseMT.
-They are 3 buttons at the top right that represent :
-
-    * Info icon: Information / Help
-    * Refresh icon: Reread the list of project
-    * New icon: Create a new presentation
-
-The middle list represent all the projects already edited once.
-When you click on a presentation, you'll have 4 icons:
-
-    * Back icon: Back to the selector screen
-    * Trash: Delete the selected project
-    * Edit: Edit the selected project
-    * View: View the selected project
-'''
-
-_help_main = '''
-The interface is divided in 4 zones:
-
-    * Left toolbar / top: You can add text and image object by
-      clicking on one of theses icons.
-    * Left toolbar / bottom:
-      - Save the project (only when changes)
-      - View the presentation
-      - Lock all objects, but not the plane
-      - Back to the selector
-    * Right toolbar:
-      - Create a new Slide
-      - Refresh all the slides captures
-'''
-
-
 class Modal(FloatLayout):
     alpha = NumericProperty(0.)
     app = ObjectProperty(None)
@@ -64,10 +24,7 @@ class ModalConfirm(Modal):
     filename = StringProperty(None)
 
 class ModalHelp(Modal):
-    text_about = StringProperty(_help_about)
-    text_help_selector = StringProperty(_help_selector)
-    text_help_main = StringProperty(_help_main)
-
+    pass
 
 class SelectorScreen(Screen):
 
